57836d51187571dbdc02817b4d0b6268bfc07eec
Steven Levithan <steves_list@hotmail.com>
2024-06-12 20:51:14 +0200

    Flag x: avoid separator postfix if not needed

--- src/flag-x.js
+++ src/flag-x.js
@@ -46,19 +46,27 @@ export function flagXPreprocessor(value, runningContext) {
     pattern += m;
     runningContext = getEndContextForIncompletePattern(pattern, runningContext);
     const {regexContext, charClassContext} = runningContext;
-    if (m === '-' && regexContext === RegexContext.CHAR_CLASS && lastSignificantCharClassContext === CharClassContext.RANGE) {
+    if (
+      m === '-' &&
+      regexContext === RegexContext.CHAR_CLASS &&
+      lastSignificantCharClassContext === CharClassContext.RANGE
+    ) {
       // Need to handle this here since the main regex-parsing code would think the hyphen forms
       // part of a subtraction operator since we've removed preceding whitespace
       throw new Error('Invalid unescaped hyphen as the end value for a range');
     }
-    if ((regexContext === RegexContext.DEFAULT && /^[?*+]\??$/.test(m)) || (regexContext === RegexContext.INTERVAL_QUANTIFIER && m === '{')) {
-      // Skip the separator prefix and connect the quantifier to the previous token. Add a
-      // separator postfix if `m` is `?` to sandbox it from follwing tokens since `?` can be a
-      // group-type marker. Ex: `( ?:)` becomes `(?(?:):)` and throws. The loop we're in matches
-      // valid group openings in one step, so we won't arrive here if matching e.g. `(?:`. Flag n
-      // precludes the need for the postfix since bare `(` is converted to `(?:`, but flag n can be
-      // turned off
-      transformed += update(m, {prefix: false, postfix: lastSignificantToken === '('});
+    if (
+      (regexContext === RegexContext.DEFAULT && /^[?*+]\??$/.test(m)) ||
+      (regexContext === RegexContext.INTERVAL_QUANTIFIER && m === '{')
+    ) {
+      // Skip the separator prefix and connect the quantifier to the previous token. This also
+      // allows whitespace between a quantifier and the `?` that makes it lazy. Add a postfix
+      // separator if `m` is `?` and we're following token `(`, to sandbox the `?` from follwing
+      // tokens (since `?` can be a group-type marker). Ex: `( ?:)` becomes `(?(?:):)` and throws.
+      // The loop we're in matches valid group openings in one step, so we won't arrive here if
+      // matching e.g. `(?:`. Flag n could prevent the need for the postfix since bare `(` is
+      // converted to `(?:`, but flag x handling always comes first and flag n can be turned off
+      transformed += update(m, {prefix: false, postfix: lastSignificantToken === '(' && m === '?'});
     } else if (regexContext === RegexContext.DEFAULT) {
       if (ws.test(m)) {
         ignoringWs = true;
